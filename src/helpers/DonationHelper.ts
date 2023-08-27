f613aef251b97d7cbd952ef92ed3f66970b7b2f7
Jeremy Zongker <jeremy@zongker.net>
2023-08-26 20:20:01 -0500

    Removed duplicate classes

--- src/helpers/DonationHelper.ts
+++
@@ -1,7 +0,0 @@
-export class DonationHelper {
-  public static getTransactionFee(amount: number) {
-    const fixedFee = 0.30;
-    const fixedPercent = 0.029;
-    return Math.round(((amount + fixedFee) / (1 - fixedPercent) - amount) * 100) / 100;
-  }
-}
