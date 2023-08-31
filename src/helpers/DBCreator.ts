56301c4441cb14c8871bd36258af35ab9c2f8abd
Jeremy Zongker <jeremy@zongker.net>
2023-08-30 20:41:30 -0500

    Updated some dependencies for 0.0.6

--- src/helpers/DBCreator.ts
+++ src/helpers/DBCreator.ts
@@ -26,7 +26,7 @@ export class DBCreator {
 
   public static async runScript(title: string, file: string, customDelimeter: boolean) {
     console.log("Creating '" + title + "'");
-    const sql = await fs.readFile(file, { encoding: "UTF-8" });
+    const sql = fs.readFileSync(file, "utf-8");
     let del = /;(?=END)\s*$|;(?!\nEND)\s*$/gm;
     if (customDelimeter) {
       del = /\$\$$/gm;
