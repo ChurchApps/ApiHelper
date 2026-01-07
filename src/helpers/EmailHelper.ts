f7c4d666b15051315c352fa14fc843e58841c063
Jeremy Zongker <jeremy@zongker.net>
2026-01-07 11:18:39 -0600

    Fixed broken email templates

--- src/helpers/EmailHelper.ts
+++ src/helpers/EmailHelper.ts
@@ -5,6 +5,10 @@ import { EnvironmentBase } from './EnvironmentBase.js';
 import { IEmailPayload } from './Interfaces.js';
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export class EmailHelper {
 
